2535690197de471a4d53e7876a8962addb32aee4
phlc <pedrocarvalho@outlook.com>
2021-12-04 12:36:11 -0300

    Fundamentals 1 - Ok

--- fundamentals/assignments.js
+++ fundamentals/assignments.js
@@ -1,13 +1,13 @@
 let country = "Brazil";
-// let continent = "South America";
+let continent = "South America";
 let population = 27;
 // console.log(country, continent, population);
-// let isIsland = false;
-// let language;
+let isIsland = false;
+let language;
 // // const language;
 // console.log(typeof country, typeof continent, typeof population);
 // console.log(typeof isIsland, typeof language);
-// language = "portuguese";
+language = "portuguese";
 // console.log(language, typeof language);
 // let half = population / 2;
 // console.log(++half);
@@ -23,15 +23,45 @@ let population = 27;
 //     end = `${33 - population} million below average`;
 // }
 // console.log(start + end);
-let a;
-a = '9' - '5'; console.log(a);
-a = '19' - '13' + '17'; console.log(a);
-a = '19' - '13' + 17; console.log(a);
-a = '123' < 57; console.log(a);
-a = 5 + 6 + '4' + 9 - 4 - 2; console.log(a);
-
-//notes
-
-let inputYear = '3';
-inputYear = Number(inputYear);
-console.log(inputYear + "4" - 2);
+// let a;
+// a = '9' - '5'; console.log(a);
+// a = '19' - '13' + '17'; console.log(a);
+// a = '19' - '13' + 17; console.log(a);
+// a = '123' < 57; console.log(a);
+// a = 5 + 6 + '4' + 9 - 4 - 2; console.log(a);
+// let numNeighbours = prompt('How many neighbours does your country have?');
+// numNeighbours = Number(numNeighbours);
+// if (numNeighbours === 1) {
+//     console.log('Only 1');
+// }
+// else if (numNeighbours === 0) {
+//     console.log('No borders');
+// }
+// else {
+//     console.log('More than 1');
+// }
+// const english = 'portuguese';
+// const pop50 = 350;
+// if (language === english && population < pop50) {
+//     console.log(`Should live in ${country}`);
+// }
+// else {
+//     console.log(`Shoulde not live in ${country}`)
+// }
+// switch (language) {
+//     case 'chinese':
+//         console.log('Most number of native speakers');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers');
+//         break;
+//     case 'english':
+//         console.log('3rd place');
+//         break;
+//     case 'portuguese':
+//         console.log('Good for you');
+//         break;
+//     default:
+//         console.log('What?');
+// }
+// console.log(`${country}'s population is ${population > 33 ? "above" : "below"} average`);
--- fundamentals/challenges.js
+++ fundamentals/challenges.js
@@ -1,19 +1,53 @@
-const markHeight = 1.69;
-const markMass = 78;
-const johnHeight = 1.88;
-const johnMass = 95;
+// const markHeight = 1.69;
+// const markMass = 78;
+// const johnHeight = 1.88;
+// const johnMass = 95;
 
-function bmi(mass, height) {
-    return mass / height ** 2;
-}
+// function bmi(mass, height) {
+//     return mass / height ** 2;
+// }
 
-const markBMI = bmi(markMass, markHeight);
-const johnBMI = bmi(johnMass, johnHeight);
-const markHigherBMI = markBMI > johnBMI;
+// const markBMI = bmi(markMass, markHeight);
+// const johnBMI = bmi(johnMass, johnHeight);
+// const markHigherBMI = markBMI > johnBMI;
 
-if (markHigherBMI) {
-    console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`);
-}
-else {
-    console.log(`John's BMI(${johnBMI}) is higher than Mark's(${johnBMI}!`);
-}
+// if (markHigherBMI) {
+//     console.log(`Mark's BMI(${markBMI}) is higher than John's(${johnBMI})!`);
+// }
+// else {
+//     console.log(`John's BMI(${johnBMI}) is higher than Mark's(${johnBMI}!`);
+// }
+// const dolphinsScores = [99, 100, 99];
+// const koalasScores = [80, 100, 80];
+// let dolphinAverage = 0;
+// let koalasAverage = 0;
+// let dolphin100 = false;
+// let koalas100 = false;
+// dolphinsScores.forEach(element => {
+//     dolphinAverage += element;
+//     if (element >= 100) {
+//         dolphin100 = true;
+//     }
+// });
+// koalasScores.forEach(element => {
+//     koalasAverage += element;
+//     if (element >= 100) {
+//         koalas100 = true;
+//     }
+// });
+
+// if (dolphinAverage > koalasAverage && dolphin100) {
+//     console.log(`Dolphins wins! 🏆`);
+// }
+// else if (koalasAverage > dolphinAverage && koalas100) {
+//     console.log('Koalas wins! 🏆');
+// }
+// else if (koalasAverage === dolphinAverage && dolphin100 && koalas100) {
+//     console.log("It's a draw");
+// }
+// else {
+//     console.log("No Trophy");
+// }
+// let bill = 200;
+// let tip;
+// console.log(`the bill was ${bill}, the tip was ${bill >= 50 && bill <= 300 ? tip = bill * 0.15 : tip = bill * 0.2} and the total value ${bill + tip}`);
