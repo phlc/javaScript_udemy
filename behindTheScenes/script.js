b87e8fae3bd32728af4ce130c388d226144e4cdb
phlc <pedrocarvalho@outlook.com>
2021-12-12 16:40:40 -0300

    behindTheScenes OK

---
+++ behindTheScenes/script.js
@@ -0,0 +1,100 @@
+'use strict';
+// const a = [];
+// console.log(a);
+// const b = document.getElementsByTagName('h1');
+// console.log(b);
+// const f = function(f2){
+//     f2('arg');
+//     console.log('function');
+// }
+// const j = (s) => console.log(`function2 ${s}`);
+// f(j);
+// j('out');
+// const g = function(){
+//     h();
+//     let tye = 2;
+//     function g2(){
+//         console.log(tye);
+//     }
+//     g2();
+// }
+// {
+//     let t = 5;
+//     console.log(t);
+// }
+// const h = function(){
+//     console.log('h');
+// }
+// g();
+// let birthYear = 1982;
+// console.log(age2(), x);
+// var x = 5;
+// const age = function(){
+//     const age = 2037 - birthYear;
+//     return age;
+// }
+// function age2(){
+//     const age = 2037 - birthYear;
+//     return age;
+// }
+// const age3 = ()=>{
+//     const age = 2037 - birthYear;
+//     birthYear = 2;
+//     return age;
+// }
+// console.log(birthYear);
+// console.log(age(), age2(), age3());
+// console.log(age, age2, age3);
+// console.log(birthYear);
+// const o = {
+//     a : ()=>console.log(this),
+//     a2 : function(){
+//         console.log(this);
+//     }, 
+//     a3 : this, 
+// }
+// o.a();
+// o.a2();
+// console.log(o.a3);
+// const a = function(){
+//     console.log(this);
+// }
+// function a2(){
+//     console.log(this);
+// }
+// a(); a2();
+// const obj = {
+//     a: 15,
+//     b: function(){
+//         console.log(this.a);
+//     },
+//     c: (a)=>console.log(a),
+// }
+// obj.b(); obj.c(obj.a);
+// const a = {};
+// console.log(a);
+// const a = function(){
+//     console.log(arguments);
+//     const b = ()=>console.log(arguments);
+//     b(2);
+//     const c = function(){
+//         console.log(arguments);
+//     }
+//     c(2);
+// }
+// a(1);
+// let a = 'a';
+// console.log(a.toUpperCase(), a);
+// const obj = {
+//     a: 'a',
+// }
+// console.log(obj.a.toUpperCase(), obj.a);
+// const obj2 = obj;
+// obj2.b = obj2.a.toUpperCase();
+// console.log(obj.b);
+// const obj3 = Object.assign({}, obj);
+// console.log(obj3);
+// obj3.b='b';
+// console.log(obj3.b, obj2.b);
+const a = {};
+console.log(a);
